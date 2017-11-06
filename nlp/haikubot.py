2620daf0550a4e16e7deb5cd73546e25b69b2adc
talikk7 <talikk@oregonstate.edu>
2017-11-06 06:01:31 -0800

    Took out typos, added sources and edited intro and methods

--- nlp/haikubot.py
+++ nlp/haikubot.py
@@ -8,6 +8,7 @@ d = cmudict.dict()
 def countSyllables(word):
 
     count = 0
+    print("Entry for " + word + ": " + str(d[word.lower()]) )
     for char in d[word.lower()]:
         for element in char:
             if element[-1] in ['0','1','2']:
