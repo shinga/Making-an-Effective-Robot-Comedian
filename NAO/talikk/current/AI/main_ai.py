809fe26f02b329bbfa9d948ef819c5e3b69efa0a
Kevin Talik <Kevin Talik>
2018-03-10 23:53:54 -0800

    Jokes are loading into the pickle, just gotta take out the test ones

--- NAO/talikk/current/AI/main_ai.py
+++ NAO/talikk/current/AI/main_ai.py
@@ -5,32 +5,26 @@ import cPickle as pickle
 
 
 '''
-# This is the range of the energy value: [0,32768]
-# Start of a joke, start sampling the energy of the microphones
-# When joke is finished, take the mean energy. If the newest sample is lower 
-#   than the mean, the currrent joke heuristics are a failure, and the discount
-#   factor is applied to the probability of global heuristics.
-#
-#   The sample is avg'd with the current shows average energy
+This is the range of the energy value: [0,32768]
+Start of a joke, start sampling the energy of the microphones
+When joke is finished, take the mean energy. If the newest sample is lower 
+than the mean, the currrent joke heuristics are a failure, and the discount
+factor is applied to the probability of global heuristics.
 
+The sample is avg'd with the current shows average energy
 After this computation, the robot needs to decide which joke to do. It computes the probability of success of each joke, and adds them to a priority queue
-
-It pops the top joke off and chooses that
-o(VlogE)
-
-
+It pops the top joke off and performs it
 '''
 class joke(object):
-    def __init__(self,_name=None,_topic=None,_length=None,_joketext=None):
-        
+    def __init__(self,_name=None,_topic=None,_length=None,_version=None,_forcedID=None,_joketext=None):
         if _name is None:
-            self.name = "" 
+            self.name = "--missing name--" 
         else:
             self.name = _name
 
         #e.x. "Romance","Aging","Self Depreciation"
         if _topic is None:
-            self.topic = ""
+            self.topic = "--missing topic--"
         else:
             self.topic = _topic
 
@@ -40,16 +34,20 @@ class joke(object):
         else:
             self.length = _length #approximate time to tell joke
 
-
         if _joketext is None:
             self.joketext = "--insert funny joke here--"
         else:
             self.joketext = _joketext #String that humans can read that tell the joke
-        #Once this joke is pushed to the joke collective, a joke ID will be give to map each joke 
-        self.id = None
+
+        #ID for behavior, and to link robot to their human versions 
+        if _forcedID is not None:
+            self.id = _forcedID
+
+        if _version is not None:
+            self.version = _version
 
     def _printInfo(self):
-    
+
         print self.name
         print self.topic
         print self.length
@@ -57,6 +55,9 @@ class joke(object):
 
         if self.id is not None:
             print self.id
+
+        if self.version is not None:
+            print self.version
 
     def _getName(self):
         return self.name
@@ -68,35 +69,24 @@ def getUserInput():
     else:
         return userWords
 
-      
-
-#Here is an example of Graphviz, and making two nodes implicitly by making an edge
 if __name__=="__main__":
-    g = Digraph('G',filename='hello.gv')
-    g.edge('Hello','World')
-    g.view()
-
-
-    #Monotonously, entering in a joke into a computer thing
-
     g = Digraph('Jokes',filename='jokeset.gv')
 
-    print "Here is where you can add jokes to a set, on a graph"
-    print "type 'q' to exit"
+    print "Here is where you can add jokes."
+    print "type 'q' to exit\n\n"
 
     qList = []
-    qList.append("Name of the joke?")
-    qList.append("Topic of the joke?")
-    qList.append("Length of Joke?")
-    qList.append("Version (Human or Robot?")
-    qList.append("ForceID (if you dont know what this is, type 'no'"
-    qList.append("Briefly ruin this joke: ")
+    qList.append("Name of the joke?") #0
+    qList.append("Topic of the joke?") #1
+    qList.append("Length of Joke?") #2
+    qList.append("Version (Human or Robot)?:") #3
+    qList.append("ForceID (if you dont know what this is, type 'no')") #4
+    qList.append("Briefly ruin this joke: ") #5
 
     jokeObjs=[]
-
     done = False
     while not done:
-
+        #get responses for the questions in qList
         respList = []
         for question in qList:
             print question
@@ -109,15 +99,17 @@ if __name__=="__main__":
                 done = True
                 break
 
-        print "Here is the response list:"
-        print respList
-
-        if len(respList) == 4:
-            jokeObjs.append(joke(respList[0],respList[1],respList[2],respList[3]))
+        if len(respList) == len(qList):
+            print "Here is the joke being appended:"
+            print respList
+     
+            jokeObjs.append(joke(respList[0],respList[1],respList[2],respList[3],respList[4],respList[5]))
         else:
             print "Joke was terminated during entry, not appending this one"
 
 
+
+    #Done adding nodes, time to append these jokes to the joke graph
     #filestring = time.strftime("%Y%m%d--%H:%M:%S") + ".p"
     filestring = "jokePickle.p"
 
@@ -127,20 +119,11 @@ if __name__=="__main__":
         pickle.dump(previousPickle,open(filestring,"wb"))
     else:
         pickle.dump(jokeObjs,open(filestring,"wb" ))
-     
 
-     
-    for noderoni in jokeObjs:
-        g.attr('node',shape='circle')
-        g.node(noderoni._getName())
-      
-    g.view()
+
+    
+
+    #g.view()
 
     print "Here is the filename:"
     print filestring
-    
-    
-        
-
-
-
--- NAO/talikk/current/AI/test.py
+++ NAO/talikk/current/AI/test.py
@@ -1,8 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
 import cPickle as pickle
 from main_ai import *
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def helloWorld():
+    print "Hello, World"
+
 temp = pickle.load(open("jokePickle.p","rb"))
-
-
 for joke in temp:
-    print "Here is a joke"
+    print "\nHere is a joke"
     joke._printInfo()
