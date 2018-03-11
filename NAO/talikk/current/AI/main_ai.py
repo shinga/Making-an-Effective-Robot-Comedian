da3495d2b487ee2eb8666106796691526b39d4eb
Kevin Talik <Kevin Talik>
2018-03-11 13:40:50 -0700

    Added a flask server prototype. Server runs, needs thread to simulate crowd

--- NAO/talikk/current/AI/main_ai.py
+++ NAO/talikk/current/AI/main_ai.py
@@ -1,20 +1,7 @@
-from graphviz import Digraph
 import time
 import os.path
 import cPickle as pickle
 
-
-'''
-This is the range of the energy value: [0,32768]
-Start of a joke, start sampling the energy of the microphones
-When joke is finished, take the mean energy. If the newest sample is lower 
-than the mean, the currrent joke heuristics are a failure, and the discount
-factor is applied to the probability of global heuristics.
-
-The sample is avg'd with the current shows average energy
-After this computation, the robot needs to decide which joke to do. It computes the probability of success of each joke, and adds them to a priority queue
-It pops the top joke off and performs it
-'''
 class joke(object):
     def __init__(self,_name=None,_topic=None,_length=None,_version=None,_forcedID=None,_joketext=None):
         if _name is None:
@@ -101,7 +88,7 @@ if __name__=="__main__":
             print "Here is the joke being appended:"
             print respList
      
-            jokeObjs.append(joke(respList[0],respList[1],respList[2],respList[3],respList[4],respList[5]))
+            jokeObjs.append([joke(respList[0],respList[1],respList[2],respList[3],respList[4],respList[5])])
         else:
             print "Joke was terminated during entry, not appending this one"
 
