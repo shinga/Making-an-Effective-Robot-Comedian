d539b6250c435d8b273e6c6228e60ba91277a882
talikk7 <talikk@oregonstate.edu>
2018-03-21 13:17:44 -0700

    Organized Files

--- NAO/talikk/current/AI/showJokes.py
+++ NAO/talikk/current/AI/showJokes.py
@@ -1,7 +1,7 @@
 from graphviz import Digraph
 import cPickle as pickle
 from main_ai import *
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 import time
 import random
@@ -9,13 +9,39 @@ from main_ai import *
 app = Flask(__name__)
 CORS(app)
 
+MAX_VAL=2
+MIN_VAL=0
 
+'''
+Prior Audience Sample, to compare levels
+0 = Weak
+1 = Medium
+2 = Strongest
+'''
+def _getSample():
+    return random.randrange(MIN_VAL,MAX_VAL + 1)
+
+prior = _getSample() #initiliaze the pryor 
+
+'''
+App Route Decorators for Flask interoperability
+'''
 
 @app.route("/")
 def helloWorld():
     print "Inside the hello world thing in test.py"
     return "Hello, World"
 
+@app.route("/pryor")
+def returnPriorValue():
+    return jsonify(pryor=prior)
+
+@app.route("/sampleValue")
+def returnTestSample():
+    return jsonify(minVal=0,maxVal=2,sample=_getSample())
+'''
+Main Function Here
+'''
 
 if __name__=="__main__":
     g = Digraph('Unconnected Jokes', filename="unconnectedJokes.gv")
@@ -31,7 +57,7 @@ if __name__=="__main__":
         counter=0
         jokeRespAvg = 0
         for x in xrange(0,random.randrange(6,12)): #Samples in a joke told
-            sample = random.randrange(0,32768) #simulated response from the microphones
+            sample = _getSample() #simulated response from the microphones
             counter = counter + 1
             jokeRespAvg=(jokeRespAvg+sample)/counter
 
@@ -46,11 +72,11 @@ if __name__=="__main__":
             done = True
         
     #prints the graph of all of the jokes
-    temp = pickle.load(open("jokePickle.p","rb"))
+    temp = pickle.load(open("smallerObjects.p","rb"))
     for joke in temp:
-        g.node(joke[0]._getName())
+        g.node(joke[0])
         print "\nHere is a joke"
-        joke[0]._printInfo()
+        print joke
 
     g.view()
 
